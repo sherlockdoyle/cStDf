4af7bcd2d6e3e61dca40a2b262239187e0af70af
Shekhar Dutta <sherlockdoyle1904+github@gmail.com>
2024-07-08 01:01:46 +0530

    Updates

---
+++ DN0eStL.js
@@ -0,0 +1,3 @@
+import{o,c as a,a as e}from"./D2a90I.js";const l={class:"markdown-body"},s=e("p",null,"Occasionally, the flag might not be in the required format. When this happens, you must reformat the flag as previously explained. Additionally, the flag might include characters that are not valid and these should be removed. (Recall the characters that are considered valid?) For example, if the flag is",-1),n=e("pre",null,[e("code",null,`flag{ex amp-le}
+`)],-1),r=e("p",null,"you should reformat it to",-1),c=e("pre",null,[e("code",null,`cStDf{example}
+`)],-1),i=e("p",null,"Are you able to find the flag for this challenge?",-1),d=[s,n,r,c,i],m={__name:"description",setup(h,{expose:t}){return t({frontmatter:{}}),(p,u)=>(o(),a("div",l,d))}};export{m as default};
---
+++ Dxi79rp.js
@@ -0,0 +1,1 @@
+const a="3193270734bf13b0b5446dbd965ad9f9",d={flagMD5:a};export{d as default,a as flagMD5};
